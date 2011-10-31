fa1fed3481dca363b84060aed4f6f73db45ca3f4
Derek <thederekjay@gmail.com>
2011-10-30 23:09:28 -0400

    ADD: New CPU Governors
    Added:
    	drivers/cpufreq/cpufreq_darkside.c (Optimized smartass by Imoseyon)
    	drivers/cpufreq/cpufreq_lagfree.c (belive this is an optimized ondemand gov?)
    	drivers/cpufreq/cpufreq_minmax.c (min and max, thats all there is to it)
    	drivers/cpufreq/cpufreq_savagedzen.c (not sure.. but has a sleep profile)
    	drivers/cpufreq/cpufreq_scary.c (Conservative gov with sleep profile)
    	drivers/cpufreq/cpufreq_smartass.c (Hacked/Optimized smartass gov)
    	drivers/cpufreq/cpufreq_smartass1.c (orginal smartass gov)
    	drivers/cpufreq/cpufreq_smartass2.c (2nd Gen. orginal smartass gov)
    	drivers/cpufreq/cpufreq_superbad.c (Low Latency Hacked/Optimized smartass gov)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -372,6 +372,33 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_MINMAX)
+extern struct cpufreq_governor cpufreq_gov_minmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_minmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
+extern struct cpufreq_governor cpufreq_gov_smartass;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS1)
+extern struct cpufreq_governor cpufreq_gov_smartass1;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass1)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCARY)
+extern struct cpufreq_governor cpufreq_gov_scary;
+#define CPUFREQ_DEFAULT_GOVERNOR    (&cpufreq_gov_scary)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SUPERBAD)
+extern struct cpufreq_governor cpufreq_gov_superbad;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_superbad)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
+extern struct cpufreq_governor cpufreq_gov_lagfree;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lagfree)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKSIDE)
+extern struct cpufreq_governor cpufreq_gov_darkside;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_darkside)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAVAGEDZEN)
+extern struct cpufreq_governor cpufreq_gov_savagedzen;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_savagedzen)
 #endif
 
 
